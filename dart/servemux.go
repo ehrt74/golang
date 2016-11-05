a5570369b121c139179f762b5774522ae33f82aa
ehrt74 <edouardtavinor@gmail.com>
2016-11-05 09:44:00 +0100

    Create servemux.go

---
+++ dart/servemux.go
@@ -0,0 +1,54 @@
+package dart
+
+import (
+	"net/http"
+	"os"
+	"bufio"
+	"strings"
+	"log"
+)
+
+var pubCache map[string]string
+func init() {
+	initPubCache()
+}
+
+func initPubCache() {
+	pubCache = make(map[string]string)
+	file, err := os.Open(".packages")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), ":")
+		pubCache[parts[0]] = parts[len(parts)-1]
+	}
+}
+
+func NewServeMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/packages/", packagesHandler)
+	for _, dir := range []string{"web", "lib"} {
+		mux.Handle("/"+dir+"/", http.FileServer(http.Dir(dir)))
+	}
+	return mux
+}
+
+func packagesHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	fileName := strings.Join(parts[2:len(parts)], "/")
+	packageName := parts[1]
+	dirName, ok := pubCache[packageName]
+	if ok {
+		http.ServeFile(w, r, dirName+fileName)
+		return
+	}
+	path := "lib/" + fileName
+	if _, err := os.Stat(path); err == nil {
+		http.ServeFile(w, r, path)
+		return
+	}
+	log.Printf("%v not found", r.URL.Path)
+}
